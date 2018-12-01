bb14c1ff11585b05337b07d2843dcb3cabe1ae76
Rhett <thedovahbrine@gmail.com>
2018-12-01 04:08:37 -0700

    meh

---
+++ Wolverine/WolverineEnviroment/Wolverine/Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public float turnSpeed = 10;
+    
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+
+            transform.Rotate(Vector3.right * Time.deltaTime);
+        }
+    }
+}
+
