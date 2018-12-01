82f414c1e22d6fcf6ee6b35c0a92cfabf8c36a82
Rhett <thedovahbrine@gmail.com>
2018-12-01 06:15:53 -0700

    sleepy, but it's done I guess

---
+++ Wolverine/WolverineEnviroment/Wolverine/Assets/butts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class butts : MonoBehaviour {
+
+
+	public int levelToLoad;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	public void LevelExit(){
+		Application.Quit();
+	}
+}
